7552f87e8ab8051f581476d4cf1cb1ee1aa62b18
Mathéo DESSAUVAGES <nebulo@Matheos-MacBook-Pro.local>
2026-04-10 10:55:57 +0200

    fix: add @EnableKafka to activate KafkaListener processing

--- src/main/java/com/talentmatch/config/KafkaConfig.java
+++ src/main/java/com/talentmatch/config/KafkaConfig.java
@@ -9,6 +9,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
@@ -21,6 +22,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class KafkaConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
